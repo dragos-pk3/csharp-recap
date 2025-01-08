6e4a71b250e144c253c0650020ba7e6849eb32ab
DESKTOP-BK38JBS\dm <drfdigital@gmail.com>
2025-01-08 16:55:05 +0200

    cmd line args

--- 1_Console_Basics/Program.cs
+++ 1_Console_Basics/Program.cs
@@ -8,19 +8,28 @@ namespace Task1
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("Enter your name:");
-            string name = Console.ReadLine() ?? string.Empty; // ?? string.Empty is a null-coalescing operator that returns the right side if the left side is null  
-
+            string name = args.Length > 0 ? args[0] : GetUserName();
+            
             Console.WriteLine($"Hello, {name}!");
             Console.WriteLine("We are glad to see you, " + name + "!");
 
-            Console.WriteLine("Enter your power level:");
+            Console.Write("Enter your power level:");
             
-            int powerLevel = int.Parse(Console.ReadLine() ?? string.Empty);
+            int powerLevel = args.Length > 1 ? int.Parse(args[1]) : int.Parse(Console.ReadLine() ?? string.Empty); // will throw an error if the input is not a number
 
             Console.WriteLine($"You claim your power level is {powerLevel}!");
 
             Console.WriteLine(powerLevel > 9000 && powerLevel < 10000 ? "Indeed, you are powerfull!" : "You're either weak or you're lying!");
+
+
         }
+
+        static string GetUserName(){
+            Console.Write("Enter your name:");
+            return Console.ReadLine() ?? string.Empty; // ?? string.Empty is a null-coalescing operator that returns the right side if the left side is null  
+            
+        }
+
+
     }
 }
