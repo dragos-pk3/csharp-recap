c22e75194468b5683732580f111522a717483c9e
DESKTOP-BK38JBS\dm <drfdigital@gmail.com>
2025-01-09 11:33:32 +0200

    simple try/catch

---
+++ 4_error_handling/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+class Program{
+    public static void Main(string[] args){
+
+
+        Console.Write("Input a number:");
+
+        try{
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"\nYou entered: {number}");
+        }
+        catch(FormatException ex){
+            Console.WriteLine($"{ex.Message}");
+            //throw;
+        }
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
